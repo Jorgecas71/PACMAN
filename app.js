29447966d758099978cd02e130443cf7a42fde51
Jorgecas71 <jorgecas71@gmail.com>
2024-06-29 21:31:25 +0200

    added js

---
+++ app.js
@@ -0,0 +1,32 @@
+// Variables para la posición del Pacman
+let pacmanX = 0;
+let pacmanY = 0;
+
+
+function movePacman() {
+
+    window.addEventListener('keydown', function (e) {
+        switch (e.key) {
+            case 'ArrowUp':
+                pacmanY -= 10;
+                break;
+            case 'ArrowDown':
+                pacmanY += 10;
+                break;
+            case 'ArrowLeft':
+                pacmanX -= 10;
+                break;
+            case 'ArrowRight':
+                pacmanX += 10;
+                break;
+        }
+
+     
+        pacman.style.left = pacmanX + 'px';
+        pacman.style.top = pacmanY + 'px';
+    });
+}
+
+window.onload = function () {
+    movePacman();
+}
